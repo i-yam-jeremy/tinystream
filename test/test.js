57e4f1dfed8a04c7981e0879f63945b91651de6e
i-yam-jeremy <i-yam-jeremy@users.noreply.github.com>
2018-11-22 12:51:58 -0800

    Started adding tests

---
+++ test/test.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+const test = require('../src/index');
+
+describe('TinyWriteStream', () => {
+	describe('#write()', () => {
+		it('should be true, dummy test', () => {
+			assert.equal(1, 1);
+		});
+	});
+});
