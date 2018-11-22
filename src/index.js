aa4cf8943cb3bc490c3192ad978c271590dc4cdc
i-yam-jeremy <i-yam-jeremy@users.noreply.github.com>
2018-11-22 12:39:35 -0800

    Encodes data using LZW and outputs bytes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 
+
 function initDictionary() {
 	let dictionary = [];
 	for (let i = 0; i < 256; i++) {
@@ -30,8 +31,31 @@ function isInDictionary(dictionary, bytePattern) {
 	return dictIndexOf(dictionary, bytePattern) != -1;
 }
 
+function lzwElementsToBytes(dictionarySize, elements) {
+	let elementBitSize = Math.ceil(Math.log2(dictionarySize));
+	let totalBitCount = elementBitSize * elements.length;
+	let byteCount = Math.ceil(totalBitCount / 8);
+
+	let bytes = new Uint8Array(byteCount);
+	
+	let bitPos = 0;
+	for (let element of elements) {
+
+		let bytePos = Math.floor(bitPos / 8);
+		let bitsUsedInByte = bitPos % 8;
+		let bitsLeftInByte = 8 - bitsUsedInByte;
+		let lowerMask = (1 << (bitsLeftInByte+1)) - 1; // all 1s (the count of 1s is bitsLeftInByte)
+		bytes[bytePos] |= (element & lowerMask) << bitsUsedInByte;
+		bytes[bytePos+1] |= element >> bitsLeftInByte;
+
+		bitPos += elementBitSize;
+	}
+
+	return bytes;
+}
+
 function compress(dictionary, data) {
-	let outputData = [];
+	let lzwElements = [];
 	let s = [];
 	for (let i = 0; i < data.length; i++) {
 		let c = data[i];
@@ -39,13 +63,14 @@ function compress(dictionary, data) {
 			s.push(c);
 		}
 		else {
-			outputData.push(dictIndexOf(dictionary, s));
+			lzwElements.push(dictIndexOf(dictionary, s));
 			dictionary.push(s.concat([c]));
 			s = [c];
 		}
 	}
-	outputData.push(dictIndexOf(dictionary, s));
-	console.log(outputData);
+	lzwElements.push(dictIndexOf(dictionary, s));
+
+	return lzwElementsToBytes(dictionary.length, lzwElements);
 }
 
 class TinyWriteStream {
@@ -81,4 +106,7 @@ class TinyReadStream {
 
 
 let out = new TinyWriteStream();
+out.on('data', (data) => {
+	console.log(data);
+});
 out.write(new Uint8Array([1, 2, 3, 3, 3, 3, 3, 3, 3, 3]));
