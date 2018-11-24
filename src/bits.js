ccc1ceeb3872d41ba90e5b6f899b5066b074c316
Jeremy Berchtold <i-yam-jeremy@users.noreply.github.com>
2018-11-24 14:50:56 -0800

    Made putNumberInBytes work with larger numbers
    TODO add more tests

--- src/bits.js
+++ src/bits.js
@@ -2,17 +2,30 @@ function getBitsPerElement(dictionaryLength) {
 	return Math.ceil(Math.log2(dictionaryLength));
 }
 
-/*
-	Works for 8 < bitCount < 16
-	TODO make this work for numbers larger than 16-bits
-*/
 function putNumberInBytes(bitCount, bitPos, n, bytes) {
+
+	let bitsLeft = bitCount;
 	let bytePos = Math.floor(bitPos / 8);
+
+	// 1. Write to the remaining bits left in the byte
 	let bitsUsedInByte = bitPos % 8;
 	let bitsLeftInByte = 8 - bitsUsedInByte;
-	let lowerMask = (1 << (bitsLeftInByte+1)) - 1; // all 1s (the count of 1s is bitsLeftInByte)
-	bytes[bytePos] |= (n & lowerMask) << bitsUsedInByte;
-	bytes[bytePos+1] |= n >> bitsLeftInByte;
+	bytes[bytePos] |= n << bitsUsedInByte; /* will get the correct number of bits needed
+		 to fill byte and the rest will be cut off cause Uint8Array */
+	n >>= bitsLeftInByte;
+	bitsLeft -= bitsLeftInByte;
+	bytePos++;
+
+	// 2. Write all full bytes
+	while (bitsLeft >= 8) {
+		bytes[bytePos] = n & 0xFF;
+		n >>= 8;
+		bitsLeft -= 8;
+		bytePos++;
+	}
+
+	// 3. Write leftover bytes to lower bits of last byte
+	bytes[bytePos] |= n;
 }
 
 function numbersToBytes(bitCountPerNum, numbers) {
--- test-src/bits.js
+++ test-src/bits.js
@@ -7,5 +7,32 @@ describe('LZW Elements To Bytes', () => {
 		it('starting singleton dictionary', () => {
 			assert.equal(getBitsPerElement(256), 8);
 		});
+		it('larger sizes', () => {
+			assert.equal(getBitsPerElement(257), 9);
+			assert.equal(getBitsPerElement(381), 9);
+			assert.equal(getBitsPerElement(511), 9);
+			assert.equal(getBitsPerElement(512), 9);
+			assert.equal(getBitsPerElement(513), 10);
+			assert.equal(getBitsPerElement(1035), 11);
+			assert.equal(getBitsPerElement(8081), 13);
+			assert.equal(getBitsPerElement(100985), 17);
+			assert.equal(getBitsPerElement(12646297897), 34);
+		});
+	});
+	describe('putNumberInBytes()', () => {
+		it('numbers smaller than 2 bytes', () => { // FIXME rename test as even 10 bits can be spread across 3 bytes
+			let bytes = new Uint8Array(6);
+			let bitPos = 0;
+
+			putNumberInBytes(9, bitPos, 385, bytes);
+			bitPos += 9;
+			assert.deepEqual(bytes, new Uint8Array([129, 1, 0, 0, 0, 0]));
+
+			putNumberInBytes(12, bitPos, 93, bytes);
+			bitPos += 12;
+			assert.deepEqual(bytes, new Uint8Array([129, 187, 0, 0, 0, 0]));
+
+			//TODO add more tests and divide into categories
+		});	
 	});
 });
